9acffe68822365f0738d81acde545c43c26609bb
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2023-11-21 18:11:59 -0500

    feat: :sparkles: add abstract classes
    Added the NumerosJava and Numeros abstract classes

--- 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
@@ -58,6 +58,41 @@ fun main() {
 
 }
 
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+    constructor(
+        uno: Int,
+        dos: Int
+    ){ //Bloque de codigo del constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+abstract class Numeros( //Constructor PRIMARIO
+    // Ejemplo:
+    // unoProp: Int, //(Parametro (sin modificador de acceso))
+    // private var uno: Int, //Propiedad Publica Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    // Propiedad de la clase protected numeros.numeroUno
+    protected val numeroUno: Int,
+    // Propiedad de la clase protected numeros.numeroDos
+    protected val numeroDos: Int,
+){
+    // var cedula: string = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init { //Bloque codigo constructor primario
+        this.numeroUno; this.numeroDos; // "this" es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+
 //void->Unit
 fun imprimirNombre(nombre: String): Unit{
     //"Nombre:"+variable+"bienvenida";
