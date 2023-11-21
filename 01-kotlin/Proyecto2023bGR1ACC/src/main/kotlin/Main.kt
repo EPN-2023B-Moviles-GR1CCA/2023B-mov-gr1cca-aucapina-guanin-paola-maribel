e7cf39999c77ff1893812abf18c9315e86b1a8c4
Paola Aucapiña <paomary.a@hotmail.com>
2023-11-21 16:19:57 -0500

    feat: :sparkles: add imprimirNombre and calcularSueldo functions
    Added examples illustrating the use of Unit, required, optional and nulleable parameters

--- 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
@@ -43,3 +43,27 @@ fun main() {
 
 
 }
+
+//void->Unit
+fun imprimirNombre(nombre: String): Unit{
+    //"Nombre:"+variable+"bienvenida";
+    println("Nombre:${nombre}")
+}
+
+fun calcularSueldo(
+    sueldo: Double, //requerido
+    tasa: Double=12.00, //opcional (defecto)
+    bonoEspecial: Double?=null, //opcion null->nullable
+):Double{
+    //Int ->Int? (nullable)
+    //String ->String? (nullable)
+    //Date ->Date? (nullable)
+
+    if (bonoEspecial==null){
+        return sueldo*(100/tasa)
+    }else{
+        bonoEspecial.dec()
+    return sueldo*(100/tasa)+bonoEspecial
+    }
+
+}
