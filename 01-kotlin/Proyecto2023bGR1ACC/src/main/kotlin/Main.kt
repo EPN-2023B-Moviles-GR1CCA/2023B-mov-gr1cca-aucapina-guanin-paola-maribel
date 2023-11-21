db12905da78544f235868cb464401db0b47e1b54
Paola Aucapiña <paomary.a@hotmail.com>
2023-11-21 15:34:16 -0500

    feat: :sparkles: add type of variables
    Added examples illustrating the use of immutable, mutable, and primitive variables in the code.

--- 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
+++ 01-kotlin/Proyecto2023bGR1ACC/src/main/kotlin/Main.kt
@@ -1,6 +1,28 @@
+import java.util.Date
+
 fun main() {
     println("Hola mundo")
-   
+    //Inmutables (NO se reasignan "=")
+    val inmutable: String ="Paola";
+    //inmutable="Paola";
 
+    //Mutables (Re asignar)
+    var mutable:String="Maribel";
+    mutable="Paola";
+
+    //val > var
+    //Duck Typing
+    var ejemploVariable="Paola Aucapiña"
+    val edadEjemplo: Int=12
+    ejemploVariable.trim()
+    //ejemploVariable=edadEjemplo
+
+    //Variable primitiva
+    val nombreProfesor: String="Adrian Eguez"
+    val sueldo: Double=1.2
+    val estadoCivil: Char='C'
+    val mayorEdad: Boolean=true
+
+    
 
 }
