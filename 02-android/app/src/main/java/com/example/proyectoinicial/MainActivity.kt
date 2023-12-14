f5e2303b2e075e2143c3e2c2610f1ecad40ccf24
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2023-12-14 17:33:03 -0500

    feat: add ListView buton
    add val botonListView

--- 02-android/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
@@ -8,11 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         val botonCicloVida=findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
             .setOnClickListener{
                 irActividad(ACicloVida::class.java)
             }
+
+        val botonListView=findViewById<Button>(R.id.btn_ir_list_view)
+
+        botonListView.setOnClickListener{
+            irActividad(BListView::class.java)
+        }
 
 
     }
@@ -23,4 +30,6 @@ class MainActivity : AppCompatActivity() {
         val intent=Intent(this,clase)
        startActivity(intent)
     }
+
+
 }
