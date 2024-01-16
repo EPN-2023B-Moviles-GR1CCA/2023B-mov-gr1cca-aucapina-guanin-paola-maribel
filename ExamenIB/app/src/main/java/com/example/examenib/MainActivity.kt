9689e6bc7388e3259a83aed5f3cc23ce370076e0
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-16 16:12:40 -0500

    Add irActividad and Snackbar functionality

--- ExamenIB/app/src/main/java/com/example/examenib/MainActivity.kt
+++ ExamenIB/app/src/main/java/com/example/examenib/MainActivity.kt
@@ -8,12 +8,14 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.examenib.ui.theme.ExamenIBTheme
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +30,22 @@ class MainActivity : AppCompatActivity() {
             }
 
     }
-    
+
     fun irActividad(clase: Class<*>){
         val intent=Intent(this, clase)
         startActivity(intent)
 
+    }
+    //Snackbar
+    fun mostrarSnackbar(texto:String){
+        Snackbar
+            .make(
+                findViewById(R.id.constrainPaises),
+                texto,
+                Snackbar.LENGTH_LONG
+
+            )
+            .show()
     }
 
 
