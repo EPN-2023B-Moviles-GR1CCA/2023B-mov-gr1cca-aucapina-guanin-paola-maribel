ecadbbd9e21f7b09009b948022a225ffd0d4bf3a
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-17 17:26:13 -0500

    Add Pais_Crear activity and layout

---
+++ ExamenIB/app/src/main/java/com/example/examenib/Pais_Crear.kt
@@ -0,0 +1,11 @@
+package com.example.examenib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pais_Crear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pais_crear)
+    }
+}
