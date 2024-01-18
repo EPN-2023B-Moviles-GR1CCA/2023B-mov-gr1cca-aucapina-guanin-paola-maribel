6c6dedbdff882ab009a4942ef7def6d7e428216f
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-18 10:21:46 -0500

    Refactor intent handling and add Snackbar

--- 02-android/app/src/main/java/com/example/proyectoinicial/CIntentExplicitoParametros.kt
+++ 02-android/app/src/main/java/com/example/proyectoinicial/CIntentExplicitoParametros.kt
@@ -1,40 +1,41 @@
 package com.example.proyectoinicial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class CIntentExplicitoParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cintent_explicito_parametros)
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+        mostrarSnackbar("${nombre} ${apellido} ${edad}")
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton
+            .setOnClickListener { devolverRespuesta() }
     }
-    override fun onCreate(savedInstanceState: BundIe?
-        super. onCreate (savedInstanceState)
-    setContentView(R. .octivitu—cintent—explicito—parametros)
-    va1 nombre = intent.getStringExtra("nombre")
-    va1 apetlido = intent.getStringExtra("apeUido")
-    va1 edad = intent.getIntExtra("edad", 0)
-    mostrarSnackbar("S{nombre} ${apellido} ${edad}")
-    val boton = findViewById<Button>(R,id.btn-devOtver-respuestO)
-    bOt이1
-    .setOnCtickListener { devotverRespuesta() }
+    fun devolverRespuesta(){
+        val intentDevolverParametros = Intent()
+        intentDevolverParametros.putExtra("nombreModificado", "Paola")
+        intentDevolverParametros.putExtra("edadModificado", 24)
+        setResult(
+            RESULT_OK, // resultado ok
+            intentDevolverParametros // variables de intent
+        ) // ponemos resultado ok y opcional retornamos variables de intent
+        finish() // cerramos la actividad
+    }
 
-    fun devoIverRespuesta(){
-        intentDevoIverParametros = lntent()
-        1ntentDevoIverParametros.putExtra("nombreModificado", "Vicente")
-        intentDevoIverParametros.putExtra("edadModificado" 33)
-        setResutt(
-            RESULT_OK, // resuttodo Ok
-            intentDevolverParametros // variobLes de intent
-        ) // ponemos resuItadO Ok 니 opcionol retornomos variabLes de intent
-        finish() // cerromos 10 octividad
-        }
-
-        fun nostrarSnackbar(texto:String)
+    fun mostrarSnackbar(texto:String){
         Snackbar
             .make(
-                findViewById(R. id . id—Loqout—intents) ,
-        texto, // texto
-        Snackbar.LENGTH—LONG // tiempo
-        .show()
+                findViewById(R.id.id_layout_intents), // view
+                texto, // texto
+                Snackbar.LENGTH_LONG // tiempo
+            )
+            .show()
+    }
 }
