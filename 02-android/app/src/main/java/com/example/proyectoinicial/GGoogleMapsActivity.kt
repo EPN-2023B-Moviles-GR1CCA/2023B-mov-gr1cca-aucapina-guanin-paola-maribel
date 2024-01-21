be94038a4606ef21bae12b2714327d9f3396723f
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-21 13:12:12 -0500

    Add location permissions and map functionality

--- 02-android/app/src/main/java/com/example/proyectoinicial/GGoogleMapsActivity.kt
+++ 02-android/app/src/main/java/com/example/proyectoinicial/GGoogleMapsActivity.kt
@@ -1,11 +1,44 @@
 package com.example.proyectoinicial
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
 
 class GGoogleMapsActivity : AppCompatActivity() {
+    private lateinit var mapa: GoogleMap
+    var permisos = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ggoogle_maps)
+
+    }//Fin OnCreate
+
+    fun solicitarPermisos(){
+        val contexto = this.applicationContext
+        val permisoFineLocation = android.Manifest.permission.ACCESS_FINE_LOCATION
+        val permisoCoarseLocation = android.Manifest.permission
+            .ACCESS_COARSE_LOCATION
+        val permisosFineLocation = ContextCompat
+            .checkSelfPermission(
+                contexto,
+                // permiso que van a checkear
+                permisoFineLocation
+            )
+        val tienePermisos = permisosFineLocation == PackageManager
+            .PERMISSION_GRANTED
+        if (tienePermisos) {
+            permisos = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this, // Contexto
+                arrayOf(  // Arreglo Permisos
+                    permisoFineLocation, permisoCoarseLocation
+                ),
+                1  // Codigo de peticion de los permisos
+            )
+        }
     }
 }
