c4c620765dfd885584fb121ab3e0650462c9ad03
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-21 08:01:40 -0500

    Add SQLite functionality to MainActivity

--- 02-android/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/proyectoinicial/MainActivity.kt
@@ -98,6 +98,12 @@ class MainActivity : AppCompatActivity() {
                 abrirActividadConParametros(
                     CIntentExplicitoParametros::class.java)
             }
+
+        val botonSqlite = findViewById<Button>(R.id.btn_sqlite)
+        botonSqlite
+            .setOnClickListener {
+                irActividad(ECrudEntrenador::class.java)
+            }
     } // Termina onCreate
 
     fun abrirActividadConParametros(
