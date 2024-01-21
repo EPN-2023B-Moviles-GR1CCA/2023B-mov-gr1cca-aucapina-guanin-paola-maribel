c68a97fa8feaa6ac0d9b527956403cbfb65f9469
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-21 08:21:34 -0500

    Add methods to create and delete trainers

--- 02-android/app/src/main/java/com/example/proyectoinicial/ESqliteHelperEntrenador.kt
+++ 02-android/app/src/main/java/com/example/proyectoinicial/ESqliteHelperEntrenador.kt
@@ -30,5 +30,41 @@ class ESqliteHelperEntrenador(
 
     }
 
+    fun crearEntrenador(
+        nombre: String,
+        descripcion: String
+    ): Boolean {
+        val basedatosEscritura = writableDatabase
+        val valoresAGuardar = ContentValues()
+        valoresAGuardar.put("nombre", nombre)
+        valoresAGuardar.put("descripcion", descripcion)
+        val resultadoGuardar = basedatosEscritura
+            .insert(
+                "ENTRENADOR", // Nombre tabla
+                null,
+                valoresAGuardar // valores
+            )
+        basedatosEscritura.close()
+        return if (resultadoGuardar.toInt() == -1) false else true
+    }
+
+    fun eliminarEntrenadorFormulario(id:Int):Boolean{
+        val conexionEscritura = writableDatabase
+        // where ID = ?
+        val parametrosConsultaDelete = arrayOf( id.toString() )
+        // [1]
+        // [1,2,3], "id=? and a=? and b=?", "id=1 and a=2 and b=3"
+        // ? = 1
+        // "id=?" => "id=1"
+        val resultadoEliminacion = conexionEscritura
+            .delete(
+                "ENTRENADOR", // Nombre tabla
+                "id=?", // Consulta Where
+                parametrosConsultaDelete
+            )
+        conexionEscritura.close()
+        return if(resultadoEliminacion == -1) false else true
+    }
+
 
 }
