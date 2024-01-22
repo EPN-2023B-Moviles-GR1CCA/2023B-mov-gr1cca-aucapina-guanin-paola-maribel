9d77424ca5b53614ed063afab09e65e94c1253b7
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-01-21 19:02:34 -0500

    Add new activities and layout files for creating and editing cities

---
+++ ExamenIB/app/src/main/java/com/example/examenib/Ciudad_Crear.kt
@@ -0,0 +1,11 @@
+package com.example.examenib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ciudad_Crear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciudad_crear)
+    }
+}
