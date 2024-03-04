c900b91acccda5f4d8f026705e410e216e42996f
Paola Aucapiña <102838577+PaolaMaribel18@users.noreply.github.com>
2024-03-04 02:44:25 -0500

    feat: Add deployment configuration file and make changes to main activity and main activity layout

--- 03-deberes/Deber3_Inkiit_RView/app/src/main/java/com/example/deber3_inkiit_rview/MainActivity.kt
+++ 03-deberes/Deber3_Inkiit_RView/app/src/main/java/com/example/deber3_inkiit_rview/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.PopupMenu
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recommendationList: ArrayList<recommendation>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
@@ -96,7 +99,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
     private fun startAutoScrollForRecommendations() {
         val handler = android.os.Handler(mainLooper)
         val autoScrollRunnable = object : Runnable {
